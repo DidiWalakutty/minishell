b3b36b54ed8001fb637b2af79e1c673cccf0d22f
Sam <samreerink@gmail.com>
2024-07-29 19:17:35 +0200

    Local commands do execute, no input enter works

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: diwalaku <diwalaku@student.42.fr>            +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/06/14 18:40:07 by diwalaku      #+#    #+#                 */
-/*   Updated: 2024/07/27 22:28:28 by sreerink      ########   odam.nl         */
+/*   Updated: 2024/07/29 19:11:35 by sreerink      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,12 @@
 
 void	error_exit(const char *msg, int status)
 {
-	if (msg)
+	if (status == 127)
+	{
+		write(STDERR_FILENO, msg, ft_strlen(msg));
+		write(STDERR_FILENO, ": command not found\n", 20);
+	}
+	else if (msg)
 		perror(msg);
 	exit(status);
 }
@@ -62,6 +67,8 @@ int	main(int argc, char **argv, char **env)
 		input = readline(SHELL_NAME);
 		if (!input)
 			error_exit("readline", EXIT_FAILURE);
+		if (!ft_strlen(input))
+			continue ;
 		data->input = input;
 		if (input != NULL)
 			add_history(data->input);
