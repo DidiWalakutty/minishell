a44dbf571c55284c5890c63456acf332f7447a43
Sam <samreerink@gmail.com>
2024-08-04 00:10:49 +0200

    Set enum to corresponding builtin cmd

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -6,17 +6,8 @@
 /*   By: sreerink <sreerink@student.codam.nl>        +#+                      */
 /*                                                  +#+                       */
 /*   Created: 2024/07/17 15:29:31 by sreerink      #+#    #+#                 */
-/*   Updated: 2024/07/17 16:04:35 by sreerink      ########   odam.nl         */
+/*   Updated: 2024/08/03 22:32:57 by sreerink      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
-
-void	cd_builtin(const char *dst_directory)
-{
-	if (chdir(dst_directory) == -1)
-	{
-		write(STDERR_FILENO, "minishell: cd: ", 15);
-		error_exit(dst_directory, EXIT_FAILURE);
-	}
-}
--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -6,16 +6,8 @@
 /*   By: sreerink <sreerink@student.codam.nl>        +#+                      */
 /*                                                  +#+                       */
 /*   Created: 2024/07/17 15:29:51 by sreerink      #+#    #+#                 */
-/*   Updated: 2024/07/17 15:30:32 by sreerink      ########   odam.nl         */
+/*   Updated: 2024/08/03 22:33:07 by sreerink      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
-
-void	echo_builtin(const char *str, bool newline)
-{
-	if (newline)
-		printf("%s\n", str);
-	else
-		printf("%s", str);
-}
