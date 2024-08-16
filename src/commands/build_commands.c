3064f986dafbee13301be8a1c1b4f78ece6c46d0
Didi Walakuttij <diwalaku@f1r5s20.codam.nl>
2024-08-16 20:59:02 +0200

    building

--- src/commands/build_commands.c
+++ src/commands/build_commands.c
@@ -6,7 +6,7 @@
 /*   By: diwalaku <diwalaku@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/08/12 17:57:27 by diwalaku      #+#    #+#                 */
-/*   Updated: 2024/08/16 20:38:34 by diwalaku      ########   odam.nl         */
+/*   Updated: 2024/08/16 20:58:39 by diwalaku      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,50 +20,15 @@ static bool	a_redirection(t_node *node)
 	return (false);
 }
 
-static bool	correct_pipe_syntax(t_node *nodes, bool *pipe_error, t_data *data)
-{
-	while (nodes)
-	{
-		if (nodes->type == PIPE)
-		{
-			if (*pipe_error)
-			{
-				// error_exit("Incorrect pipes\n", STDERR_FILENO);
-				data->exit_status = 2;
-				return (1);
-			}
-			*pipe_error = true;
-		}
-		else
-			*pipe_error = false;
-		nodes = nodes->next;
-	}
-	return (*pipe_error);
-}
-
-static int	correct_pipe_usage(t_node *nodes, t_data *data)
-{
-	t_node	*head;
-	bool	pipe_error;
-
-	head = nodes;
-	pipe_error = false;
-	if (correct_pipe_syntax(nodes, &pipe_error, data) == false)
-		return (1);
-	return (0);
-}
-
 // Checks for space-input only and
 // pipe_usage.
-static int	ready_for_building(t_node *nodes, t_data *data, char *input)
+static int	not_just_spaces(t_node *nodes)
 {
 	int		count;
 	bool	flag;
-	t_node	*head;
 
 	count = 0;
 	flag = false;
-	head = nodes;
 	while (nodes)
 	{
 		if (nodes->type == SEPARATOR)
@@ -73,9 +38,12 @@ static int	ready_for_building(t_node *nodes, t_data *data, char *input)
 	}
 	if (flag && count == 1)
 		return (0);
-	if (!correct_pipe_usage(head, data))
-		return (0);
 	return (1);
+}
+
+t_cmd	merge_commands(t_node *nodes, t_data *data)
+{
+	
 }
 
 t_cmd	*build_commands(t_node *nodes, t_data *data)
@@ -83,8 +51,9 @@ t_cmd	*build_commands(t_node *nodes, t_data *data)
 	t_cmd	*commands;
 
 	commands = NULL;
-	if (nodes && ready_for_building(nodes, data, data->input))
+	if (nodes && not_just_spaces(nodes))
 	{
+		commands = merge_commands(nodes, data);
 		printf("syntax is alright\n");
 	}
 	return (commands);
