1a30c9228b4edb2f92650e6cda6443b90415b192
Didi Walakuttij <diwalaku@f0r3s25.codam.nl>
2024-09-13 17:11:15 +0200

    Added the check_cmd_builtin

--- src/commands/build_inits_and_nodes.c
+++ src/commands/build_inits_and_nodes.c
@@ -6,7 +6,7 @@
 /*   By: diwalaku <diwalaku@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/08/23 15:45:42 by diwalaku      #+#    #+#                 */
-/*   Updated: 2024/09/12 19:18:36 by diwalaku      ########   odam.nl         */
+/*   Updated: 2024/09/13 17:09:56 by diwalaku      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,21 @@ void	init_redirects(t_cmd *cmd)
 	cmd->redir_out = NULL;
 }
 
-t_cmd	*init_cmds(void)
+t_cmd	*init_cmds(t_data *data)
 {
 	t_cmd	*command;
 
 	command = mem_check(malloc(sizeof(t_cmd)));
 	command->cmd = NULL;
+	command->pid = 0;
+	command->path = NULL;
 	command->args = NULL;
+	command->env = data->env;
 	command->redir_in = NULL;
 	command->redir_out = NULL;
 	command->pipe_in = false;
 	command->pipe_out = false;
+	command->builtin = NO_BUILTIN;
 	command->next = NULL;
 	return (command);
 }
